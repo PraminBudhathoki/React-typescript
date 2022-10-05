05b9fcf3035263eae737f88a8180d8e178591d7c
PraminBudhathoki <pramin2059@gmail.com>
2022-10-06 00:23:27 +0545

    1st day of typescript learned

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to Dr.Vipin Classes for learning React with typescript</h2>
+      <h3>
+        We also have other 40+ playlist for learning android,kotlin,React and
+        etc.
+      </h3>
     </div>
   );
 }
