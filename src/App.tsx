a84110c898d6f9b558eec85f712b417721694012
PraminBudhathoki <pramin2059@gmail.com>
2022-10-06 22:53:44 +0545

    Day 2 learned

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
+import ChannelInfo from "./Component/ChannelInfo";
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to Dr.Vipin Classes for learning React with typescript</h2>
-      <h3>
-        We also have other 40+ playlist for learning android,kotlin,React and
-        etc.
-      </h3>
+      <ChannelInfo channelName="Dr.Vipin Classes" playlistCount={40} />
     </div>
   );
 }
---
+++ src/Component/ChannelInfo.tsx
@@ -0,0 +1,20 @@
+// type ChannelProps = {
+//   channelName: string;
+// };
+interface ChannelProps {
+  channelName: string;
+  playlistCount: number;
+}
+function ChannelInfo(props: ChannelProps) {
+  return (
+    <div>
+      <h2>Welcome to {props.channelName} for learning React with typescript</h2>
+      <h3>
+        We also have other {props.playlistCount}+ playlist for learning
+        android,kotlin,React and etc.
+      </h3>
+    </div>
+  );
+}
+
+export default ChannelInfo;
